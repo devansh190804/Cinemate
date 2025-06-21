cd711cfb8e4436e2b909b5700ad53644cfdda399
devansh190804 <devanshpandit2004@gmail.com>
2025-06-21 13:50:41 +0530

    deployment

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -7,6 +7,7 @@ import { useTitle } from "../hooks/useTitle"
 export const MovieDetail = () => {
   const params = useParams();
   const[movie, setMovie] = useState({});
+  // eslint-disable-next-line
   const pageTitle = useTitle(movie.title);  
 
    const image = movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : Backup ;
@@ -18,7 +19,7 @@ export const MovieDetail = () => {
         setMovie(json)
        }
        fetchMovie();
-   },[]);
+   },[params.id]);
 
 
   return (
